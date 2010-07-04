a6b650c956c9d6bc61bcc1ebf6df257bbd259b0a
Pairing Machine <pairing@edgecase.com>
2010-07-04 16:26:04 -0400

    started integrating the youtube gem into the project

---
+++ config/initializers/youtube.rb
@@ -0,0 +1,1 @@
+#youtube = YouTube::Client.new 'AI39si6LJxy8r8V_APOcGId8iuyBNt9-MhhJ03qRFLP2uWgiQ_JPKVnE5y3z5zXFTrWXU74Gr2pq4333O9SZm261RyQQmbOYUg' # Get one here: <http://youtube.com/my_profile_dev>. 

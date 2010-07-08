a2378b1d3836ec64b8b42494487651a294ac591d
Pairing Machine <pairing@edgecase.com>
2010-07-07 23:29:40 -0400

    removed authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   layout 'application'
-  before_filter :authenticate
-  USER_ID, PASSWORD = "resolute", "buckeyes44"
-  
-
-private
-   def authenticate
-      if Rails.env == "production"
-        authenticate_or_request_with_http_basic do |id, password| 
-            id == USER_ID && password == PASSWORD
-        end
-      end
-   end
 end

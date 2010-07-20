61903d6fc619d177b2f627fa88915eba206b1881
Pairing Machine <pairing@edgecase.com>
2010-07-20 03:08:37 -0400

    Changed subject line in contact mailer emails
    - set performing deliveries in development mode to false

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -4,7 +4,7 @@ class ContactMailer < ActionMailer::Base
   def contact_email(message)
    @message = message
    @url = "http://example.com/login" 
-   mail(:to => "resolutelacrosse@gmail.com",  :subject => "Welcome to My Awesome Site") do |format| 
+   mail(:to => "resolutelacrosse@gmail.com",  :subject => "Message from Contact Us Form on Resolute Lacrosse") do |format| 
      format.text { render 'contact_email'  } 
    end 
   end
@@ -12,7 +12,7 @@ class ContactMailer < ActionMailer::Base
   def response_email(message)
    @message = message
    @url = "http://example.com/login" 
-   mail(:to => @message.email,  :subject => "Welcome to My Awesome Site") do |format| 
+   mail(:to => @message.email,  :subject => "Thank you for contacting us!") do |format| 
      format.html { render 'response_email'  } 
      format.text { render 'response_email'  } 
    end 

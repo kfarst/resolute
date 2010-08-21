bd7fe334cee83ce083a30492e8544c37bc97fe00
Pairing Machine <pairing@edgecase.com>
2010-08-21 02:36:38 -0400

    trying another migration fix

--- db/migrate/20100821025929_add_columns_to_items.rb
+++ db/migrate/20100821025929_add_columns_to_items.rb
@@ -1,15 +1,15 @@
 class AddColumnsToItems < ActiveRecord::Migration
   def self.up
+    add_column :items, :camp_registration_form, :boolean, :default => false
     add_column :items, :slug, :string
-    add_column :items, :camp_registration_form, :boolean, :default => 0
 
     Item.transaction do
-      Item.all.each{|i| i.slug = i.title.downcase.parameterize;i.camp_registration_form = 0;i.save}
+      Item.all.each{|i| i.slug = i.title.downcase.parameterize;i.camp_registration_form = false;i.save}
     end
   end
 
   def self.down
-    remove_column :items, :slug
     remove_column :items, :camp_registration_form
+    remove_column :items, :slug
   end
 end

cfa877a6d0f686fbc49593918568f232e4500712
Pairing Machine <pairing@edgecase.com>
2010-09-24 09:23:42 -0400

    contest entry form validation fix

--- app/models/contest_entry.rb
+++ app/models/contest_entry.rb
@@ -1,8 +1,7 @@
 class ContestEntry < ActiveRecord::Base
-  validates_presence_of :first_name, :last_name, :email, :age, :position, :school
+  validates_presence_of :name, :email, :age, :position, :school
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
   validates_uniqueness_of :email
-  validates_length_of :age, :maximum => 2, :message => "may only be two digits (zero in front if less than 10)"
   validates_numericality_of :age
 
   def to_param

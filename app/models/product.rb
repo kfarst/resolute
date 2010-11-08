9fb9ce3abd54b6c721b6815c583c9ff7ef0f0c02
Pairing Machine <pairing@edgecase.com>
2010-11-07 23:58:20 -0500

    add popup window for product pictures in store
    - resize product images

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ActiveRecord::Base
-  has_attached_file :picture, :styles => { :medium => "400x300", :thumb => "300x200" }
+  has_attached_file :picture, :styles => { :medium => "400x300", :thumb => "250x150" }
   validates_presence_of :name, :price
   validates_numericality_of :price
   validates_format_of :size, :with => /(?:^|,)(\\\"(?:[^\\\"]+|\\\"\\\")*\\\"|[^,]*)/

6b0ab7e733c91ff5be7a526d780140f279d6a268
Pairing Machine <pairing@edgecase.com>
2010-12-09 23:34:48 -0500

    postgres way of altering registration_form_id type from string to integer

--- db/migrate/20101210033219_change_registration_form_id_from_string_to_integer_on_camp_registrations.rb
+++ db/migrate/20101210033219_change_registration_form_id_from_string_to_integer_on_camp_registrations.rb
@@ -1,9 +1,37 @@
 class ChangeRegistrationFormIdFromStringToIntegerOnCampRegistrations < ActiveRecord::Migration
   def self.up
-    change_column :camp_registrations, :registration_form_id, :integer
+    CampRegistration.transaction do
+      rename_column :camp_registrations, :registration_form_id, :id_string
+      add_column :camp_registrations, :registration_form_id, :integer
+
+      say "Transferring data to new column..."
+
+      CampRegistration.all.each do |camp_registration|
+        camp_registration.registration_form_id = camp_registration.id_string.to_i
+        camp_registration.save!
+      end
+
+      say "Done."
+
+      remove_column :camp_registrations, :id_string
+    end
   end
 
   def self.down
-    change_column :camp_registrations, :registration_form_id, :string
+    CampRegistration.transaction do
+      rename_column :camp_registrations, :registration_form_id, :id_integer
+      add_column :camp_registrations, :registration_form_id, :string
+
+      say "Transferring data to old column..."
+
+      CampRegistration.all.each do |camp_registration|
+        camp_registration.registration_form_id = camp_registration.id_integer.to_s
+        camp_registration.save!
+      end
+
+      say "Done."
+
+      remove_column :camp_registrations, :id_integer
+    end
   end
 end

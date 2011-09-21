a47b9827245643f9fdf062f5435cb3ef3e733709
Kevin Farst <farst.6@osu.edu>
2011-09-20 23:01:21 -0400

    fix for panel URL regex

--- app/models/panel.rb
+++ app/models/panel.rb
@@ -14,7 +14,7 @@ class Panel < ActiveRecord::Base
   validates_presence_of :title, :if => :has_any_attributes?
   validates_presence_of :url, :if => :has_any_attributes?
   validates_presence_of :panel, :if => :has_any_attributes?
-  validates_format_of :url, :with => /^([a-z0-9]+)+(-[a-z0-9]+)*$/i, :message => "must be in the format of 'sample-page'", :if => :has_any_attributes?
+  validates_format_of :url, :with => /^[\w\/-]+$/i, :message => "was not in the proper format (must contain only letters, numbers, dashes, and forward slashes)", :if => :has_any_attributes?
   validates_attachment_presence :panel, :if => lambda { self.panel? }
 
   def has_any_attributes?
